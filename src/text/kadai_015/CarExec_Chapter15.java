fd0689e71d4ac0b61fb7811dec1c56c9815f9efd
nakanishil <nakanishi.jsdf@gmail.com>
2024-10-22 14:07:06 +0900

    kadai_015の提出

---
+++ src/text/kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,9 @@
+package text.kadai_015;
+
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		Car_Chapter15 car = new Car_Chapter15(3);
+		
+		car.run();
+	}
+}
---
+++ src/text/kadai_015/Car_Chapter15.java
@@ -0,0 +1,32 @@
+package text.kadai_015;
+
+public class Car_Chapter15 {	
+	// 内部データ
+	private int gear = 1; // ギア　1～5速を表す
+	private int speed = 10;// ギアチェンジ後の速度を表す
+	
+//	初期化
+	public Car_Chapter15(int gear) {
+		this.gear = gear;
+	}
+	
+//	ギアの値により速度を変える
+	public void gearChange(int afterGear) {
+		switch(gear) {
+		case 1  -> this.speed = 10;
+		case 2  -> this.speed = 20;
+		case 3  -> this.speed = 30;
+		case 4  -> this.speed = 40;
+		case 5  -> this.speed = 50;
+		default -> this.speed = 10;
+		}
+	}
+	
+//	ギアチェンジ後の速度を表示
+	public void run() {
+		System.out.println("ギア：" + this.gear + "　速度：" + this.speed);
+	}
+		
+}
+
+
